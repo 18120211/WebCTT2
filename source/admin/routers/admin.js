a04df4586f9f258060ae45830ef7c0887e07c57f
ptbminh1108 <ptbminh1108@users.noreply.github.com>
2021-01-06 00:37:36 +0700

    show lecturers list and course list in admin

--- source/admin/routers/admin.js
+++ source/admin/routers/admin.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 const Admin = require("../models/Admin.model");
 
+const Lecturer = require("../models/Lecturer.model");
+
+const Course = require("../models/Course.model");
+
 const mongoose = require('mongoose');
 
 const bcrypt = require("bcryptjs");
@@ -20,7 +24,7 @@ const {
   ensureAuthenticated,
   forwardAuthenticated
 } = require("../config/auth_admin");
-const Lecturer = require("../models/Lecturer.model");
+
 
 // Home Page
 
@@ -37,6 +41,74 @@ router.get("/account/edit", ensureAuthenticated,  (req, res) => {
     })
 });
 
+router.get("/lecturer/lecturersList", ensureAuthenticated,  (req, res) => {
+   
+    Lecturer.find({}, function(err, Lecturers) {
+        let data = [];
+        let Lecturers_arr = [];
+
+    
+        Lecturers.forEach(function(Lecturer) {
+
+            Lecturers_arr.push(Lecturer);
+
+        });
+
+        data['Lecturers'] = Lecturers_arr;
+        
+        res.render("admin/lecturer/lecturersList", {
+            user: req.user, data:data
+        })
+    });
+
+    
+});
+
+router.get("/course/coursesList", ensureAuthenticated,  (req, res) => {
+   
+    Course.find({}, function(err, Courses) {
+        let data = [];
+        let Courses_arr = [];
+
+    
+        Courses.forEach(function(Course) {
+
+            Courses_arr.push(Course);
+
+        });
+
+        data['Courses'] = Courses_arr;
+        
+        res.render("admin/course/coursesList", {
+            user: req.user, data:data
+        })
+    });
+
+    
+});
+
+router.post("/account/edit", async  (req, res) => {
+    console.log(req.user.email);
+    Admin.findOne({email:req.user.email}).then((user)=>{
+        if(user){
+            user.name = req.body.name;
+            user.gender = req.body.gender;
+            user.description = req.body.description;
+            user.avatar = req.body.avatar;
+            user.save();
+        }
+    });
+    Lecturer.findOne({email:req.user.email}).then((user)=>{
+        if(user){
+            user.name = req.body.name;
+            user.gender = req.body.gender;
+            user.description = req.body.description;
+            user.avatar = req.body.avatar;
+            user.save();
+        }
+    });
+    res.redirect("/admin/account/edit");
+});
 router.get("/", ensureAuthenticated,  (req, res) => {
     console.log(req.user);
     res.render("admin/common/home", {
@@ -77,12 +149,18 @@ router.get("/register", function (req, res) {
 
 router.get("/is-user-available", function (req, res) {
     Admin.findOne({email:req.query.email}).then((user)=>{
-    if(user){
-      res.json(false);
-    }else{
-      res.json(true);
-    }
-  })
+        if(user){
+            res.json(false);
+            return;
+        }
+    });
+    Lecturer.findOne({email:req.query.email}).then((user)=>{
+        if(user){
+          res.json(false);
+          return;
+        }
+    });
+    res.json(true);
 });
 
 router.post("/register",async function (req, res) {
@@ -139,7 +217,6 @@ router.post('/upload', function (req, res) {
         console.log(err);
       } else {
         const avatar =  ('/public/avatar/') + req.user._id.toString() + '/' + 'avatar.png';
-
         res.json(avatar);
       }
     });
